b4c8706cd3333c192c5089fe4f6023fd10b11f35
julRus <julia_russell4@outlook.com>
2019-10-29 09:48:02 +0000

    Forgot to commit work for a while, all seeding completed along with tests

---
+++ db/migrations/20191028115705_create_table_name_nc_news_comments.js.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+  console.log("table comments created...");
+  return knex.schema.createTable("comments", commentsTable => {
+    commentsTable.increments("comment_id").primary();
+    commentsTable.text("body").notNullable();
+    commentsTable.integer("votes").defaultTo(0);
+    commentsTable.integer("article_id").references("articles.article_id");
+    commentsTable.string("author").references("users.username");
+    commentsTable.timestamp("created_at").defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex) {
+  console.log("removing comments table...");
+  return knex.schema.dropTable("comments");
+};
--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,41 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+  // console.log(list);
+  if (!list) return undefined;
+  const newList = JSON.parse(JSON.stringify(list));
+  newList.map(obj => {
+    const date = new Date(obj.created_at);
+    return (obj.created_at = date);
+  });
 
-exports.makeRefObj = list => {};
+  // const date = new Date(newList[0].created_at);
+  // newList[0].created_at = date;
+  // return newList;
+  return newList;
+};
 
-exports.formatComments = (comments, articleRef) => {};
+exports.makeRefObj = list => {
+  if (!list) return undefined;
+  const ref = JSON.parse(JSON.stringify(list));
+  const refObj = {};
+  ref.map(obj => {
+    return (refObj[obj.title] = obj.article_id);
+  });
+  return refObj;
+};
+
+exports.formatComments = (articleRef, comments) => {
+  const commentsData = JSON.parse(JSON.stringify(comments));
+  commentsData.map(obj => {
+    const date = new Date(obj.created_at);
+    obj.created_at = date;
+    obj["author"] = obj.created_by;
+    delete obj.created_by;
+    const article = articleRef[obj.belongs_to];
+    obj.article_id = article;
+    delete obj.belongs_to;
+    return obj;
+  });
+  // console.log(commentsData);
+  // console.log(commentsData);
+  return commentsData;
+};
